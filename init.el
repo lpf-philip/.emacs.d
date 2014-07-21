f584e7148f68abc46be639301afde63b57f8c1b4
philip <lpfrylzx@gmail.com>
2014-07-22 02:04:25 +0800

    add gtags keymap

--- init.el
+++ init.el
@@ -575,11 +575,23 @@ point reaches the beginning or end of the buffer, stop there."
 
 ;; gtags
 (autoload 'gtags-mode "gtags" "" t)
+(add-hook 'gtags-mode-hook
+          '(lambda ()
+                                        ; Local customization (overwrite key mapping)
+             (define-key gtags-mode-map "\C-f" 'scroll-up)
+             (define-key gtags-mode-map "\C-b" 'scroll-down)
+             ))
+(add-hook 'gtags-select-mode-hook
+          '(lambda ()
+             (setq hl-line-face 'underline)
+             (hl-line-mode 1)
+             ))
 (add-hook 'c-mode-hook
 	  '(lambda ()
 	     (gtags-mode t)
 	     ))
-
+(setq gtags-suggested-key-mapping t)
+(setq gtags-auto-update t)
 
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
